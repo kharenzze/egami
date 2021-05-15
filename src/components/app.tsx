e821230809b77994fa66a33042526226b7eef917
Paolo <str237@gmail.com>
2021-05-15 16:31:12 +0200

    Remove header

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,12 +3,10 @@ import { Route, Router } from 'preact-router';
 
 import Home from '../routes/home';
 import NotFoundPage from '../routes/notfound';
-import Header from './header';
 
 const App: FunctionalComponent = () => {
     return (
         <div id="preact_root">
-            <Header />
             <Router>
                 <Route path="/" component={Home} />
                 <NotFoundPage default />
