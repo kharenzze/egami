8648cfa48172c01cba913f5e36d8c62d1d9217e5
Paolo <str237@gmail.com>
2021-05-15 20:10:21 +0200

    Button

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,11 +5,17 @@ const Title = jsx('h1', {
     color: 'red'
 })
 
+const Upload = jsx('button', {})
+
 const Home: FunctionalComponent = () => {
+    const onClick: () => void = () => {
+        console.log('click')
+    }
     return (
         <div>
             <Title>Home</Title>
             <p>This is the Home component.</p>
+            <Upload onClick={onClick}>Upload</Upload>
         </div>
     );
 };
