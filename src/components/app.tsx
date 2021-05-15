8ac5faf71ba45f0f101c8387915960a24b624a92
Paolo <str237@gmail.com>
2021-05-15 16:30:17 +0200

    Remove some parts

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,7 +2,6 @@ import { FunctionalComponent, h } from 'preact';
 import { Route, Router } from 'preact-router';
 
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 import NotFoundPage from '../routes/notfound';
 import Header from './header';
 
@@ -12,8 +11,6 @@ const App: FunctionalComponent = () => {
             <Header />
             <Router>
                 <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
                 <NotFoundPage default />
             </Router>
         </div>
